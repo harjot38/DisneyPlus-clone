8d1e2afdbc8194568f96958f8e9fc5eebf7f12c4
harjot38 <144142200+harjot38@users.noreply.github.com>
2025-05-20 10:22:31 -0600

    updated slider

--- disney-clone/src/Components/Slider.jsx
+++ disney-clone/src/Components/Slider.jsx
@@ -38,11 +38,11 @@ function Slider() {
 
     return (
         <div>
-            <HiChevronDoubleLeft className="hidden md:block text-[30px] absolute mx-15 mt-[155px] cursor-pointer" onClick={sliderLeft}/>
-            <HiChevronDoubleRight className="hidden md:block text-[30px] absolute mx-15 mt-[155px] cursor-pointer right-0" onClick={sliderRight}/>
+            <HiChevronDoubleLeft className="hidden md:block text-[30px] absolute mx-15 mt-[355px] cursor-pointer" onClick={sliderLeft}/>
+            <HiChevronDoubleRight className="hidden md:block text-[30px] absolute mx-15 mt-[355px] cursor-pointer right-0" onClick={sliderRight}/>
             <div className='flex overflow-x-auto w-full px-20 py-6 no-scrollbar scroll-smooth' ref={elementRef}>
                 {movieList.map((item) => (
-                    < img src={image_base_url + item.backdrop_path} className='min-w-full md:h-[300px] object-cover object-left-top mr-7 rounded-lg hover:border-[4px] border-gray-300 transition-all duration-100 ease-in' />
+                    < img src={image_base_url + item.backdrop_path} className='min-w-full md:h-[500px] object-cover object-left-top mr-7 rounded-lg hover:border-[4px] border-gray-300 transition-all duration-100 ease-in' />
                 ))}
             </div>
 
